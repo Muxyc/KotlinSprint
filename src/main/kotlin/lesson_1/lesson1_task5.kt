566e4da78299a2a6ac247565292c85f9531eee51
Muxyc <muxyc@yandex.ru>
2023-11-16 16:31:24 +0300

    KS-1-5 Information about how much Gagarin spent time in space

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main(){
+    val seconds: Short = 6480
+    val hour: String = "01"
+    val minutes: String = "48"
+    val secondsRemainder:  String = "00"
+    println("$hour:$minutes:$secondsRemainder")
+
+}
