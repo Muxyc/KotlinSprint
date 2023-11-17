38bab447621246081044f087b5f0dda612afe938
Muxyc <muxyc@yandex.ru>
2023-11-17 13:53:31 +0300

    KS-2-4 Calculation bonus ore

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,15 @@
+package lesson_2
+
+fun main() {
+
+    val oreIron: Int = 11
+    val oreCrystal: Int = 7
+    val oreBuff: Int = 20
+    val oreIronBonus: Int
+    val oreCrystalBonus: Int
+
+    oreIronBonus=oreIron*oreBuff/100
+    println("oreIronBonus:$oreIronBonus")
+    oreCrystalBonus=oreCrystal*oreBuff/100
+    println("oreСrystalBonus:$oreCrystalBonus")
+}
