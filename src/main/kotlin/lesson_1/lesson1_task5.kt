ebfcb226ab0dc87694c474fd1498599f384376c6
Muxyc <muxyc@yandex.ru>
2023-11-17 09:54:51 +0300

    KS-1-5 Доработана с учетом высчитывания часов, минут и секунд программно

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,17 @@
 package lesson_1
 
 fun main(){
-    val seconds: Short = 6480
-    val hour: String = "01"
-    val minutes: String = "48"
-    val secondsRemainder:  String = "00"
-    println("$hour:$minutes:$secondsRemainder")
+    val seconds: Int = 6480
+    val hour: Int
+    val minutes: Int
+    val secondsRemainder: Int
+
+    hour=seconds/3600
+    minutes=(seconds-3600*hour)/60
+    secondsRemainder=(seconds-3600*hour-60*minutes)/60
+
+    if (hour < 10) print("0$hour:") else print("$hour:")
+    if (minutes < 10) print("0$minutes:") else print("$minutes:")
+    if (secondsRemainder < 10) println("0$secondsRemainder") else println("$secondsRemainder")
 
 }
