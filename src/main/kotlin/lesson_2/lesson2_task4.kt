19da2396a545f30e8185980ee73230bcbf611752
Muxyc <muxyc@yandex.ru>
2023-11-18 11:43:11 +0300

    KS-2-4 Применил автоформат кода

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,15 +1,14 @@
 package lesson_2
 
 fun main() {
-
     val oreIron: Int = 11
     val oreCrystal: Int = 7
     val oreBuff: Int = 20
     val oreIronBonus: Int
     val oreCrystalBonus: Int
 
-    oreIronBonus=oreIron*oreBuff/100
+    oreIronBonus = oreIron * oreBuff / 100
     println("oreIronBonus:$oreIronBonus")
-    oreCrystalBonus=oreCrystal*oreBuff/100
+    oreCrystalBonus = oreCrystal * oreBuff / 100
     println("oreСrystalBonus:$oreCrystalBonus")
 }
