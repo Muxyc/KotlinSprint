bdec1f810fc1017859c6cb1a81805039e77221b8
Muxyc <muxyc@yandex.ru>
2023-11-18 11:31:52 +0300

    KS-2-3 Вынес количество минут в часу и количество часов в сутках в константы

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,16 @@
 package lesson_2
 
+const val hoursPerDay = 24
+const val minutesPerHour = 60
 fun main() {
     val trainDepartureHour: Int = 9
     val trainDepartureMinute: Int = 39
-    val trainDeparture: Int = trainDepartureHour * 60 + trainDepartureMinute
+    val trainDeparture: Int = trainDepartureHour * minutesPerHour + trainDepartureMinute
     val trainTravelMinute: Int = 457
     val trainComing: Int = trainDeparture + trainTravelMinute
-    val trainComingHour: Int = if (trainComing / 60 < 24) trainComing / 60 else trainComing / 60 -24
-    val trainComingMinute: Int = trainComing % 60
+    val trainComingHour: Int =
+        if (trainComing / minutesPerHour < hoursPerDay) trainComing / minutesPerHour else trainComing / minutesPerHour - hoursPerDay
+    val trainComingMinute: Int = trainComing % minutesPerHour
 
     println("$trainComingHour:$trainComingMinute")
 
