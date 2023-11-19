9edaee5d61f80d7cadb7f08d4616c9afaf824ae6
Muxyc <muxyc@yandex.ru>
2023-11-19 13:25:52 +0300

    KS-3-2 Вывод в консоль [ФИО, возраст]

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main(){
+    val personLastNameOld: String = "Андреева"
+    val personFirstNameOld: String = "Татьяна"
+    val personPatronymicOld: String = "Сергеевна"
+    val personalAegOld: Int = 20
+    val personLastName: String = "Сидорова"
+    val personFirstName: String = "Татьяна"
+    val personPatronymic: String = "Сергеевна"
+    val personalAeg: Int = 22
+
+    println(personLastNameOld+" "+personFirstNameOld+" "+personPatronymicOld+", "+personalAegOld)
+    println(personLastName+" "+personFirstName+" "+personPatronymic+", "+personalAeg)
+
+}
