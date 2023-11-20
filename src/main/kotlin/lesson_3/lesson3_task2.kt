e464d67c69b0b08e77679ab98616b3d135e6217c
Muxyc <muxyc@yandex.ru>
2023-11-20 14:43:36 +0300

    Переделал код на меньшее количество переменных.

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,16 +1,13 @@
 package lesson_3
 
-fun main(){
-    val personLastNameOld: String = "Андреева"
-    val personFirstNameOld: String = "Татьяна"
-    val personPatronymicOld: String = "Сергеевна"
-    val personalAegOld: Int = 20
-    val personLastName: String = "Сидорова"
+fun main() {
+    var personLastName: String = "Андреева"
     val personFirstName: String = "Татьяна"
     val personPatronymic: String = "Сергеевна"
-    val personalAeg: Int = 22
+    var personalAeg: Int = 20
 
-    println(personLastNameOld+" "+personFirstNameOld+" "+personPatronymicOld+", "+personalAegOld)
-    println(personLastName+" "+personFirstName+" "+personPatronymic+", "+personalAeg)
-
+    println(personLastName + " " + personFirstName + " " + personPatronymic + ", " + personalAeg)
+    personLastName = "Сидорова"
+    personalAeg = 22
+    println(personLastName + " " + personFirstName + " " + personPatronymic + ", " + personalAeg)
 }
