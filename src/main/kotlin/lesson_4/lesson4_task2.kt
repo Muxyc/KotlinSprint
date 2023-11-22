064bf4f3f8fccae84aef3fa5a6baaffe4a763313
Muxyc <muxyc@yandex.ru>
2023-11-22 20:42:37 +0300

    Исправил найминг констант на общепринятое

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,8 +1,8 @@
 package lesson_4
 
-const val averageMinWt = 35
-const val averageMaxWt = 100
-const val averageMaxVolume = 100
+const val AVERAGE_MIN_WT = 35
+const val AVERAGE_MAX_WT = 100
+const val AVERAGE_MAX_VOLUME = 100
 
 fun main() {
     var cargoWt = 20
@@ -10,12 +10,12 @@ fun main() {
 
     println(
         "Груз весом $cargoWt кг и объемом $cargoVolume л соответствует категории 'Average':" +
-                "${cargoWt > averageMinWt && cargoWt <= averageMaxWt && cargoVolume < averageMaxVolume}"
+                "${cargoWt > AVERAGE_MIN_WT && cargoWt <= AVERAGE_MAX_WT && cargoVolume < AVERAGE_MAX_VOLUME}"
     )
     cargoWt = 50
     cargoVolume = 100
     println(
         "Груз весом $cargoWt кг и объемом $cargoVolume л соответствует категории 'Average':" +
-                "${cargoWt > averageMinWt && cargoWt <= averageMaxWt && cargoVolume < averageMaxVolume}"
+                "${cargoWt > AVERAGE_MIN_WT && cargoWt <= AVERAGE_MAX_WT && cargoVolume < AVERAGE_MAX_VOLUME}"
     )
 }
