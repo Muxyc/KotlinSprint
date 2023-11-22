54df84932289f84f611a411a10e7d5448e9e56f4
Muxyc <muxyc@yandex.ru>
2023-11-22 09:26:31 +0300

    Заменен способ разбивки строки на функцию split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,14 +2,7 @@ package lesson_3
 
 fun main() {
     var stepInfo: String = "D2-D4;0"
-    var positionStart: String
-    var positionEnd: String
-    var stepNumber: Int
+    var stepInfoList: List<String> = stepInfo.split('-', ';')
 
-    positionStart = stepInfo.substring(0, 2)
-    positionEnd = stepInfo.substring(3, 5)
-    stepNumber = stepInfo.substringAfterLast(';').toInt()
-    println(positionStart)
-    println(positionEnd)
-    println(stepNumber)
+    for (i in stepInfoList) println(i)
 }
